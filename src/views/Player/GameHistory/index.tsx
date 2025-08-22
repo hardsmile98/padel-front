14d6f132888c5a5ed581bd62ff04273012c09148
Denis <d.kotelev@carprice.tech>
2025-08-22 18:11:53 +0300

    feat

---
+++ src/views/Player/GameHistory/index.tsx
@@ -0,0 +1,19 @@
+import { Container } from "@/src/components"
+import styles from './styles.module.css'
+
+const GameHistory = () => {
+  return (
+    <section 
+        className={styles.gameHistory} 
+        id="game-history"
+    >
+        <Container>
+            <h2 className={styles.title}>
+                История игр в лиге
+            </h2>
+        </Container>
+    </section>
+  )
+}
+
+export default GameHistory
--- src/views/Player/index.tsx
+++ src/views/Player/index.tsx
@@ -1,10 +1,15 @@
 import { GetPlayerBySlugResponse } from '@/src/types'
 import About from './About'
+import GameHistory from './GameHistory'
 
 const Player = ({ data }: { data: GetPlayerBySlugResponse }) => {
 
   return (
-    <About data={data} />
+    <>
+      <About data={data} />
+
+      <GameHistory />
+    </>
   )
 }
 
